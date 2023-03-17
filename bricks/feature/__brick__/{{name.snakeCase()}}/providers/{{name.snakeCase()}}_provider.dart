2fa1fa25f2e0de23f0f2b84fe281d58250e77fdd
Shashi Sharma <sharmashashi940@gmail.com>
2023-03-17 14:09:39 +0545

    add riverpod generator for provider

---
+++ bricks/feature/__brick__/{{name.snakeCase()}}/providers/{{name.snakeCase()}}_provider.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part '{{name.snakeCase()}}.g.dart';
+
+
+@riverpod
+class {{name.pascalCase()}} extends _${{name.pascalCase()}}{
+  @override
+  build(){
+    //TODO
+  }
+}
